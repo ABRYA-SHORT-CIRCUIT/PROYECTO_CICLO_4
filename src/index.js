2d9fb37747a5d689be0e464a246da01c9297a5ee
YoryinaOrtegaUparela <109432483+YoryinaOrtegaUparela@users.noreply.github.com>
2022-10-27 16:23:09 -0500

    Integración 1-trabajo Alex -Yoryina

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//Para la alerta
+const options = {
+
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+
+  transition: transitions.SCALE
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
